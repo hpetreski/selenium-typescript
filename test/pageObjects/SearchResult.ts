6f4de3105b0a0b45f85542da7733c70a8b71d777
Hristijan Petreski <drpinceca@outlook.com>
2023-07-08 21:15:13 +0200

    Adding the page object structure to the project

---
+++ test/pageObjects/SearchResult.ts
@@ -0,0 +1,13 @@
+import { By, Locator, WebDriver, until } from 'selenium-webdriver';
+
+export class SearchResult{
+    private searchContents: Locator = By.id('contents')
+    
+    constructor(private driver: WebDriver){}
+
+    async getContentsElement(){
+        await this.driver.wait(until.elementLocated(this.searchContents), 5000);
+
+        return await this.driver.findElement(this.searchContents);
+    }
+}
