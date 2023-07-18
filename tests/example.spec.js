d77cdde22b93711ed3e109f580f706168988708f
Hristijan Petreski <petreski.hristijan@gmail.com>
2023-07-18 22:26:30 +0200

    Initial commit

---
+++ tests/example.spec.js
@@ -0,0 +1,31 @@
+import { By, Builder, Browser } from 'selenium-webdriver';
+import assert from 'assert';
+
+describe('First script', function () {
+  let driver;
+
+  before(async function () {
+    driver = await new Builder().forBrowser('chrome').build();
+  });
+
+  after(async () => await driver.quit());
+
+  it('First Selenium script', async function () {
+    await driver.get('https://www.selenium.dev/selenium/web/web-form.html');
+
+    let title = await driver.getTitle();
+    assert.equal("Web form", title);
+
+    await driver.manage().setTimeouts({ implicit: 500 });
+
+    let textBox = await driver.findElement(By.name('my-text'));
+    let submitButton = await driver.findElement(By.css('button'));
+
+    await textBox.sendKeys('Selenium');
+    await submitButton.click();
+
+    let message = await driver.findElement(By.id('message'));
+    let value = await message.getText();
+    assert.equal("Received!", value);
+  });
+});
